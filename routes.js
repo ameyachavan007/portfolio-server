f51e5754acc18a57a3fd8aad499e41b943e63e18
bhavikakarale <chavanameya007@gmail.com>
2024-02-10 17:11:00 -0500

    added route for home page

--- routes.js
+++ routes.js
@@ -45,6 +45,21 @@ redisClient.on('error', (err) => {
   console.error("Redis Connection Error:", err);
 });
 
+router.post("/home", async (req, res) => {
+  const {username} = req.body;
+  try{
+    let user = await User.findOne({username});
+    if(user){
+      return res.status(200).json({message: "Username exists"});
+    }
+    else{
+      return res.status(404).json({error: "Username not found"});
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
 //Sign-up route
 router.post("/signup", async (req, res) => {
   const { email, username } = req.body;
